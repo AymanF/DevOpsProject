e4f59b715d74f6aa700fb81e0251fdea5523450a
AymanF <aymanfattar@gmail.com>
2018-12-06 17:20:26 +0100

    Revert "Add files via upload"
    This reverts commit 792ffa0a6dcf27708358da513496b3187c28d0b5.

--- Project_Task4/DBConn.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$ErrorMsgs = array();
-	$DBConnect = new mysqli("localhost","root","", "efrei");
-	if  (!$DBConnect)
-		$ErrorMsgs[] = "The database server is not available.";
-	else
-		return 1;
-
-?>
